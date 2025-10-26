124c7f3c2a34cd275ea8b35e650ff12fee6e66c2
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-10-26 19:17:03 +0530

    Added new premium procedure for data layer verification

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -5,6 +5,7 @@ import { headers } from "next/headers";
 import { initTRPC, TRPCError } from "@trpc/server";
 
 import { auth } from "@/lib/auth";
+import { polarClient } from "@/lib/polar";
 
 export const createTRPCContext = cache(async () => {
   /**
@@ -43,3 +44,23 @@ export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
 
   return next({ ctx: { ...ctx, auth: session } });
 });
+
+export const premiumProcedure = protectedProcedure.use(
+  async ({ ctx, next }) => {
+    const customer = await polarClient.customers.getStateExternal({
+      externalId: ctx.auth.user.id,
+    });
+
+    if (
+      !customer.activeSubscriptions ||
+      customer?.activeSubscriptions?.length === 0
+    ) {
+      throw new TRPCError({
+        code: "FORBIDDEN",
+        message: "Active subscriptions required",
+      });
+    }
+
+    return next({ ctx: { ...ctx, customer } });
+  }
+);
