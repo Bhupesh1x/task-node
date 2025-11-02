07baab4c07b5ac117753f4c5b8e0889ba03d784a
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-11-03 00:01:14 +0530

    Added react-flow and initial nodes to editor component

--- src/features/editor/components/Editor.tsx
+++ src/features/editor/components/Editor.tsx
@@ -1,19 +1,78 @@
 "use client";
 
+import {
+  type Node,
+  type Edge,
+  type NodeChange,
+  type EdgeChange,
+  type Connection,
+} from "@xyflow/react";
+import {
+  addEdge,
+  MiniMap,
+  Controls,
+  ReactFlow,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+} from "@xyflow/react";
+import { useState, useCallback } from "react";
+
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/useWorkflows";
 
 import { ErrorView, LoadingView } from "@/components/EntityComponents";
+
+import "@xyflow/react/dist/style.css";
 
 interface Props {
   workflowId: string;
 }
 
+const initialNodes = [
+  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
+  { id: "n2", position: { x: 0, y: 100 }, data: { label: "Node 2" } },
+];
+const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
+
 export function Editor({ workflowId }: Props) {
   const workflow = useSuspenseWorkflow(workflowId);
 
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
+  );
+  const onConnect = useCallback(
+    (params: Connection) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    []
+  );
+
   return (
-    <div>
-      <h1>{JSON.stringify(workflow, null, 2)}</h1>
+    <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        proOptions={{
+          hideAttribution: true,
+        }}
+      >
+        <Background />
+        <Controls />
+        <MiniMap />
+      </ReactFlow>
     </div>
   );
 }
