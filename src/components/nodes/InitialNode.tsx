eea61dbc320e59aab3af48e5a40316e24dfc9069
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-11-04 23:42:33 +0530

    Created reusable workflow-node component

--- src/components/nodes/InitialNode.tsx
+++ src/components/nodes/InitialNode.tsx
@@ -2,15 +2,18 @@ import { memo } from "react";
 import { PlusIcon } from "lucide-react";
 import type { NodeProps } from "@xyflow/react";
 
+import { WorkflowNode } from "./WorkflowNode";
 import { PlaceholderNode } from "../placeholder-node";
 
 function InitialNodeComponent(props: NodeProps) {
   return (
-    <PlaceholderNode {...props} onClick={() => {}}>
-      <div className="flex items-center justify-center cursor-pointer">
-        <PlusIcon className="size-4" />
-      </div>
-    </PlaceholderNode>
+    <WorkflowNode>
+      <PlaceholderNode {...props} onClick={() => {}}>
+        <div className="flex items-center justify-center cursor-pointer">
+          <PlusIcon className="size-4" />
+        </div>
+      </PlaceholderNode>
+    </WorkflowNode>
   );
 }
 
---
+++ src/components/nodes/WorkflowNode.tsx
@@ -0,0 +1,51 @@
+import { NodeToolbar, Position } from "@xyflow/react";
+import { Button } from "../ui/button";
+import { SettingsIcon, Trash2Icon } from "lucide-react";
+
+interface Props {
+  name?: string;
+  description?: string;
+  showToolbar?: boolean;
+  children: React.ReactNode;
+  onDelete?: () => void;
+  onSettings?: () => void;
+}
+
+export function WorkflowNode({
+  name,
+  description,
+  showToolbar = true,
+  children,
+  onDelete,
+  onSettings,
+}: Props) {
+  return (
+    <>
+      {showToolbar && (
+        <NodeToolbar>
+          <Button variant="ghost" size="sm" onClick={onSettings}>
+            <SettingsIcon className="size-4" />
+          </Button>
+          <Button variant="ghost" size="sm" onClick={onDelete}>
+            <Trash2Icon className="size-4" />
+          </Button>
+        </NodeToolbar>
+      )}
+      {children}
+      {!!name && (
+        <NodeToolbar
+          isVisible
+          position={Position.Bottom}
+          className="text-center max-w-[200px]"
+        >
+          <p className="font-medium">{name}</p>
+          {!!description && (
+            <p className="text-muted-foreground text-sm truncate">
+              {description}
+            </p>
+          )}
+        </NodeToolbar>
+      )}
+    </>
+  );
+}
