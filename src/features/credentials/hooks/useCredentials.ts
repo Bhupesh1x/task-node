2908ecdcfee841d920c88757fe6890e7880e350c
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-11-27 00:24:47 +0530

    Feat: created hooks for create-credential, remove-credential and suspense-credential

--- src/features/credentials/hooks/useCredentials.ts
+++ src/features/credentials/hooks/useCredentials.ts
@@ -1,4 +1,9 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import { toast } from "sonner";
 
 import { useTRPC } from "@/trpc/client";
 
@@ -9,4 +14,56 @@ export function useSuspenseCredentials() {
 
   const [params] = useCredentialsParams();
   return useSuspenseQuery(trpc.credentials.getMany.queryOptions(params));
+}
+
+export function useCreateCredential() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.credentials.create.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential "${data?.name}" created`);
+
+        setTimeout(() => {
+          queryClient.invalidateQueries(
+            trpc.credentials.getMany.queryOptions({})
+          );
+        }, 100);
+      },
+      onError: (error) => {
+        toast.error(`Failed to create credential: ${error?.message}`);
+      },
+    })
+  );
+}
+
+export function useRemoveCredential() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    trpc.credentials.remove.mutationOptions({
+      onSuccess: (data) => {
+        toast.success(`Credential "${data.name}" deleted`);
+        queryClient.invalidateQueries(
+          trpc.credentials.getMany.queryOptions({})
+        );
+        queryClient.invalidateQueries(
+          trpc.credentials.getOne.queryOptions({ id: data.id })
+        );
+      },
+      onError: () => {
+        toast.error(
+          `Failed to delete credential. Please try again after some time.`
+        );
+      },
+    })
+  );
+}
+
+export function useSuspenseCredential(id: string) {
+  const trpc = useTRPC();
+
+  return useSuspenseQuery(trpc.credentials.getOne.queryOptions({ id }));
 }
