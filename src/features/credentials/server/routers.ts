95c1c8eb1f360622959ecc1ec8e55bd56767c9c5
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-11-26 22:02:55 +0530

    Feat: Created new route for getmany credentials and fixed getone by id router

--- src/features/credentials/server/routers.ts
+++ src/features/credentials/server/routers.ts
@@ -6,6 +6,7 @@ import {
   protectedProcedure,
 } from "@/trpc/init";
 import { db } from "@/lib/db";
+import { PAGINATION } from "@/configs/constants";
 import { CredentialType } from "@/generated/prisma";
 
 export const credentialsRouters = createTRPCRouter({
@@ -50,11 +51,59 @@ export const credentialsRouters = createTRPCRouter({
       })
     )
     .query(({ ctx, input }) => {
-      return db.workflow.findFirstOrThrow({
+      return db.credential.findFirstOrThrow({
         where: {
           id: input.id,
           userId: ctx.auth.user.id,
         },
       });
+    }),
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(PAGINATION.defaultPage),
+        pageSize: z
+          .number()
+          .min(PAGINATION.minPageSize)
+          .max(PAGINATION.maxPageSize)
+          .default(PAGINATION.defaultPageSize),
+        search: z.string().default(""),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { page, pageSize, search } = input;
+
+      const [credentials, totalCount] = await Promise.all([
+        db.credential.findMany({
+          where: {
+            userId: ctx.auth.user.id,
+            name: { contains: search, mode: "insensitive" },
+          },
+          take: pageSize,
+          skip: (page - 1) * pageSize,
+          orderBy: {
+            updatedAt: "desc",
+          },
+        }),
+        db.credential.count({
+          where: {
+            userId: ctx.auth.user.id,
+            name: { contains: search, mode: "insensitive" },
+          },
+        }),
+      ]);
+
+      const totalPages = Math.ceil(totalCount / pageSize);
+      const hasNextPage = page < totalPages;
+      const hasPrevPage = page > 1;
+
+      return {
+        items: credentials,
+        totalPages,
+        hasNextPage,
+        hasPrevPage,
+        page,
+        pageSize,
+      };
     }),
 });
