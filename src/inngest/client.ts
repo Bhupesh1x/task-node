5d27d4e7adb4f17d79b58e581b18f2c469c2e992
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-12-02 23:58:52 +0530

    Fix: Added signingKey for inngest

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -5,4 +5,5 @@ export const inngest = new Inngest({
   id: "task-node",
   middleware: [realtimeMiddleware()],
   eventKey: process.env.INNGEST_EVENT_KEY,
+  signingKey: process.env.INNGEST_SIGNING_KEY,
 });
